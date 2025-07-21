02b8e4876d7f493c5e89a4d18049d19be701c574
Cesar Rodrigo Nieto Lopez <cesar_rnl@hotmail.com>
2025-07-21 14:40:14 -0600

    refactor: Improve GetUserId to use HttpContext

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -1,10 +1,28 @@
-﻿namespace ManagerMoney.Services
+﻿using System.Security.Claims;
+
+namespace ManagerMoney.Services
 {
     public class UserServices : IUserServices
     {
+        private readonly HttpContext httpContext;
+
+        public UserServices(IHttpContextAccessor httpContextAccessor)
+        {
+            httpContext = httpContextAccessor.HttpContext;
+        }
         public int GetUserId()
         {
-            return 1;
+            if (httpContext.User.Identity.IsAuthenticated)
+            {
+                var idClaim = httpContext.User.Claims.Where(x=> x.Type == ClaimTypes.NameIdentifier).FirstOrDefault();
+                var id = int.Parse(idClaim.Value);
+                return id;
+            }
+            else
+            {
+                throw new ApplicationException("El usuario no esta autenticado");
+            }
+            
         }
     }
 
